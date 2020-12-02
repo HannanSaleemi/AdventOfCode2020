206ff452193be074c81ad5af0749557a8beaf8c5
Hannan Saleemi <hannansaleemi1@gmail.com>
2020-12-02 12:18:32 +0000

    Pushing aprt 2 as i fogot to

--- Day1-ReportRepair/day1.py
+++ Day1-ReportRepair/day1.py
@@ -23,3 +23,26 @@ while (left < right):
         left += 1
 
 
+# Three Numbers
+# Static pointer going through all, and then Two Pointers on the rest
+for staticPointer in range(0, len(inputArr) - 2):
+    tripleSum = 2020 - inputArr[staticPointer]
+    if tripleSum > 0:
+        left = 0
+        right = len(inputArr)-1
+        while (left < right):
+            if (left == staticPointer):
+                left += 1 
+                continue
+            tempSum = inputArr[left] + inputArr[right] + inputArr[staticPointer]
+            if (tempSum == 2020):
+                print("Three Numbers Are: {}, {}, {}".format(inputArr[left], inputArr[right], inputArr[staticPointer]))
+                print(inputArr[left] * inputArr[right] * inputArr[staticPointer])
+                break
+            elif (tempSum > 2020):
+                right -= 1
+            else:
+                left += 1
+    else:
+        continue
+            
